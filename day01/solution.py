1aa149d1d0049cb56eb50139510a2e22a0d3b359
Mateusz Kopczyski <mateusz.kopczynski@tietoevry.com>
2024-12-07 16:30:24 +0100

    Solution for day 1 part 2

--- day01/solution.py
+++ day01/solution.py
@@ -5,7 +5,7 @@ numbers_column1 = []
 numbers_column2 = []
 for line in lines:
     numbers = line.strip().split()
-    
+
     for i, num in enumerate(numbers):
         if i % 2 == 0:
             numbers_column1.append(int(num))
@@ -15,9 +15,24 @@ for line in lines:
 numbers_column1.sort()
 numbers_column2.sort()
 
-total = 0
+totalPart1 = 0
 
 for i in range(len(numbers_column1)):
-    total = total + abs(numbers_column1[i] - numbers_column2[i])
+    totalPart1 += abs(numbers_column1[i] - numbers_column2[i])
 
-print(total)
+#Result 1 day part 1
+print(f"Total part 1: {totalPart1}")
+
+totalPart2 = 0
+
+for i in range(len(numbers_column1)):
+    number = numbers_column1[i]
+    totalSameNumbers = 0
+
+    for i in range(len(numbers_column2)):
+
+        if numbers_column2[i] == number:
+            totalSameNumbers += 1
+    totalPart2 += number * totalSameNumbers
+
+print(f"Total part 2: {totalPart2}")
